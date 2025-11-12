e9d8e2798a9ca53be68537e97ff342cfe604d3b0
abhilekh <dm@travelogy.in>
2025-11-12 16:57:57 +0530

    Initial desktop app

---
+++ vite.config.js
@@ -0,0 +1,40 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import electron from 'vite-plugin-electron';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    electron([
+      {
+        entry: 'src/electron/main.js',
+        vite: {
+          build: {
+            outDir: 'dist-electron', 
+            rollupOptions: {
+              output: {
+                entryFileNames: 'main.js',
+              },
+            },
+          },
+        },
+        onstart(options) {
+          options.startup();
+        },
+      },
+    ]),
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+  ],
+  build: {
+    outDir: 'dist-react'
+  },
+  base: './',
+  server: {
+    port: 4000,
+    strictPort: true 
+  }
+}):
